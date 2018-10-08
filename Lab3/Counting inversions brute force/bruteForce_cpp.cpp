12e312091391921696ec126e8c00a3f17c69a80e
jcmonterrosas <38740763+jcmonterrosas@users.noreply.github.com>
2018-10-08 04:22:54 -0500

    Update bruteForce_cpp.cpp

--- Lab3/Counting inversions brute force/bruteForce_cpp.cpp
+++ Lab3/Counting inversions brute force/bruteForce_cpp.cpp
@@ -6,35 +6,20 @@
 
 using namespace std;
 
-//void lectura();
-
 int main()
 {
     long n;
     cin >> n;
     vector <unsigned long long> arr(n);
-    //long i;
-    /*for(i = 0;i < n;i++)
-    {
-    cin >> arr[i];
-    }*/
     FILE *p;
     fstream in;
-    //ifstream archivo;
-    //string texto;
     long long i = 0;
     in.open("input.txt", ios::in);
     in >> arr[i];
-    //archivo.open("input.txt",ios::in);
     while(!in.eof()){
             i++;
             in>>arr[i];
-            //cout << arr[i];
     }
-    //fclose(p);
-    //for(i =0;i<n;i++){
-      //  cout << arr[i]<<endl;
-    //}
 
     long long j,k,count=0;
 
@@ -44,8 +29,6 @@ int main()
         {
             if(arr[i] > arr[i+1])
             {
-                //cout<<arr[i]<<endl;
-                //cout<<arr[i+1]<<endl;
                 long long temp;
                 cout<<count<<endl;
                 count++;
@@ -57,20 +40,6 @@ int main()
         }
     }
     cout << count;
-    //lectura();
 
     return 0;
 }
-/*
-void lectura(long long arr[]){
-    ifstream archivo;
-    string texto;
-    long count = 0;
-
-    archivo.open("input.txt",ios::in);
-    while(!archivo.eof()){
-            getline(archivo,texto);
-            arr[count] = texto;
-    }
-    archivo.close();
-}*/
